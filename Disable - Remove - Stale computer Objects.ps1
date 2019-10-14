8785d133de654ed2228555679eaaedba50c30c08
Gabrielle Liberato <40310291+gabiliberato@users.noreply.github.com>
2019-10-14 11:35:28 -0300

    Update Disable - Remove - Stale computer Objects.ps1

--- Disable - Remove - Stale computer Objects.ps1
+++ Disable - Remove - Stale computer Objects.ps1
@@ -1,12 +1,4 @@
-#Script:	Stale_AD_Objects.vbs
-#Purpose:  To check AD for stale computer objects based on date logon criteria and disable / delete
-#Author:   Paperclips	
-#Email:	pwd9000@hotmail.co.uk
-#Date:     Oct 2013
-#Comments: Can be scheduled to run e.g. weekly to eleviate manual checks
-#Notes:  
-
-$1year = (Get-Date).AddDays(-365) # The 365 is the number of days from today since the last logon.
+ $1year = (Get-Date).AddDays(-365) # The 365 is the number of days from today since the last logon.
 $1y1m = (Get-Date).AddDays(-395)
 
 # Disable computer objects and move to disabled OU (Older than 1 year):
