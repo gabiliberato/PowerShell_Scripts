3222067e6d3a1c4aea0ec2fce1ef6a15f5df28e1
Gabrielle Liberato <40310291+gabiliberato@users.noreply.github.com>
2019-12-26 11:46:14 -0300

    Add files via upload

---
+++ ComputersRemoveTemp.ps1
@@ -0,0 +1,23 @@
+﻿<#
+    Script: ComputersRemoveTemp.ps1
+    Propósito: Remover máquinas desativados no AD por "x" tempo definido por $datecutoff
+        1. Máquinas desativados no AD
+        2. Definição de tempo.
+#>
+
+#Variavel de tempo
+$datecutoff = (Get-Date).AddDays(-30)
+
+#Busca de Máquinas desabilitadas do AD
+#Parametro de LastLogonDate
+#Retorno de Informações dos usuário
+#Exportar lista para arquivo csv
+
+Get-ADComputer -Filter {(Enabled -eq $False)} -Properties LastLogonDate | where {$_.LastLogonDate -lt $datecutoff} | Select-Object Name, SamAccountName, LastLogonDate | Out-File C:\Users\gabrielle\Desktop\Scripts\DisableComputersTemp_Script.csv 
+
+#Get-ADComputer -Filter {(Enabled -eq $False)} -Properties LastLogonDate | where {$_.LastLogonDate -lt $datecutoff} | Select-Object Name, SamAccountName, LastLogonDate | Remove-Computer -Confirm $True
+
+$computers = Get-ADComputer -Filter {(Enabled -eq $False)} | where {$_.LastLogonDate -lt $datecutoff} 
+
+Write-Host "Quantidade de Máquinas Desativados: $($computers.count)"
+
