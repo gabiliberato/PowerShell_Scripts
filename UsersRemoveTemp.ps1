19212aaeec1713e84fd21934401263d596e5f3ed
Gabrielle Liberato <40310291+gabiliberato@users.noreply.github.com>
2019-12-26 10:22:46 -0300

    Add files via upload

---
+++ UsersRemoveTemp.ps1
@@ -0,0 +1,23 @@
+﻿<#
+    Script: UsersRemoveTemp.ps1
+    Propósito: Remover usuários desativados no AD por "x" tempo definido por $datecutoff
+        1. Usuários desativados no AD
+        2. Definição de tempo.
+#>
+
+#Variavel de tempo
+$datecutoff = (Get-Date).AddDays(-60)
+
+#Busca de usuários desabilitados do AD
+#Parametro de LastLogonDate
+#Retorno de Informações dos usuários
+Search-ADAccount -UsersOnly -AccountDisable |where {$_.LastLogonDate -lt $datecutoff} |Select-Object Name, DistinguishedName,Enabled, UserPrincipalName,LastLogonDate 
+
+#Remoção de usuários 
+#Search-ADAccount -UsersOnly -AccountDisable |where {$_.LastLogonDate -lt $datecutoff} |Select-Object Name, DistinguishedName,Enabled, UserPrincipalName,LastLogonDate | Remove-ADUser -Confirm
+
+#Variavel de Parametros
+$users = Search-ADAccount -UsersOnly -AccountDisable | where {$_.LastLogonDate -lt $datecutoff} 
+
+#Quantidade de usuários desabilitados
+Write-Host "Quantidade de Usuários Desativados: $($users.count)"
